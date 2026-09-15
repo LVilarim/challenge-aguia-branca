e035e7defe1ca3d38da55ac688248d2346cbd71a
vilarim <lucasvilarim.souza@gmail.com>
2026-09-14 23:23:18 -0300

    feat: first commit

---
+++ src/test/java/com/challenge/aguiabranca/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.aguiabranca.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
